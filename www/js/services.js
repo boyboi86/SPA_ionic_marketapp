728779e6bfa06ee8006fc40278b714d63bb4d05f
boyboi86 <boyboi86@gmail.com>
2016-11-26 21:48:32 +0800

    http call to API added

--- www/js/services.js
+++ www/js/services.js
@@ -1,1 +1,21 @@
-angular.module('newApp.services', []);
+angular.module('newApp.services', [])
+
+.factory('stockDataService', function($q, $http){
+
+var getPriceData = function(){
+
+  var deferred = $q.defer();
+  var url = "http://finance.yahoo.com/webservice/v1/symbols/YHOO/quote?bypass=true&format=json&view=detail";
+
+  $http.get("http://finance.yahoo.com/webservice/v1/symbols/YHOO/quote?bypass=true&format=json&view=detail")
+    .then(function(jsonData){
+      console.log(jsonData.data.list.resources[0].resource.fields);
+    });
+
+};
+
+
+  return {
+    getPriceData: getPriceData
+  };
+});
